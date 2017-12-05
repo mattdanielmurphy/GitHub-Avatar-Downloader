b9a9620c1377584080c7151182d77d7d86ecf2bd
Matthew Daniel Murphy <matt@mattmurphy.ca>
2017-12-05 12:17:47 -0500

    implemented downloadImageByURL into getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -2,37 +2,38 @@ var request = require('request');
 var token = require('./secrets');
 var fs = require('fs');
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-// 	var options = {
-// 		url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
-// 		headers: {
-// 			'User-Agent': 'request',
-// 			'Authorization': `token ${token.GITHUB_TOKEN}`
-// 		}
-// 	};
+function getRepoContributors(repoOwner, repoName, cb) {
+	var options = {
+		url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+		headers: {
+			'User-Agent': 'request',
+			'Authorization': `token ${token.GITHUB_TOKEN}`
+		}
+	};
 		
-// 	request(options, function(err, res, body) {
-// 		cb(err, body);
-// 	});
-// }
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-// 	console.log("Errors:", err);
-// 	var newResult = JSON.parse(result);
-// 	for (var i in newResult) {
-// 		if (newResult[i].avatar_url) {
-// 			console.log(newResult[i].avatar_url)
-// 		}
-// 	}
-// });
-
-var path = './test.whatever';
+	request(options, function(err, res, body) {
+		cb(err, body);
+	});
+}
 
 function downloadImageByURL(url, filePath) {
 	request.get(url)
 		.on('error', function(err) { throw err })
 		.pipe(fs.createWriteStream(filePath))
 }
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+	console.log("Errors:", err);
+	let parsedResult = JSON.parse(result);
+	for (var i in parsedResult) {
+		if (parsedResult[i]['avatar_url']) {
+			let loginName = parsedResult[i]['login'];
+			let avatarPath = `avatars/${loginName}.jpg`;
+			let avatarURL = parsedResult[i]['avatar_url'];
+			// console.log(avatarPath);
+			downloadImageByURL(avatarURL, avatarPath);
+		}
+	}
+});
